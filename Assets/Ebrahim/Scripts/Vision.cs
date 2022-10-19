eab6d4b8df0a4da151450ec9d95da4df1d2b5e70
ebrahimsi <hhooaa2013@gmail.com>
2022-10-19 15:33:28 +0300

    day3

--- Assets/Ebrahim/Scripts/Vision.cs
+++ Assets/Ebrahim/Scripts/Vision.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +6,68 @@ using UnityEngine;
 public class Vision : MonoBehaviour
 {
    [SerializeField] private LayerMask obs;
+    private float timer=3;
+    [SerializeField] private GameObject exclamationSprite;
+    bool isshowing=true;
+    //[SerializeField] EnemyAi techer;
     // Start is called before the first frame update
+
     private void OnTriggerStay(Collider other)
     {
+        Debug.Log("Trigger");
         if (other.gameObject.tag == "Player") 
         {
              Vector3 DirictionTarget = (other.gameObject.transform.position - transform.position).normalized;
             float distance = Vector3.Distance(transform.position, other.gameObject.transform.position);
-    
-            if (Physics.Raycast(transform.position, DirictionTarget, obs))
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position,DirictionTarget);
+           
+            if (Physics.Raycast(ray, out hit))
             {
-                Debug.Log("i see you");
+                 Debug.DrawRay(transform.position,hit.transform.position,Color.red);
+                Debug.Log(hit.transform.name);
+                if (hit.transform.CompareTag("Player")) {
+                    Debug.Log("i see you");
+                    //
+                    if (isshowing) {
+                        isshowing = false;
+                        exclamationSprite.active = true;
+                        Invoke("ShowSprite", 1f);
+                    }
+                    timer -= 1* Time.deltaTime;
+                    if (timer <= 0) 
+                    {
+                        Debug.Log("Stop");
+                    }
+                    
+                }
+                else
+                {
+                    isshowing = true;
+                    if (!(timer <= 0))
+                    {
+                        timer = 3;
+                    }
+                    Debug.Log("where are you");
+                }
+
             }
-            else 
+           
+        }
+    }
+
+    private void ShowSprite()
+    {
+        exclamationSprite.active = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player") 
+        {
+            if (!(timer<=0)) 
             {
-                Debug.Log("where are you");
+                timer = 3;
             }
         }
     }
